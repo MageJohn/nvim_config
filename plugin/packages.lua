d7b1ff114f7059369febe7811b2a613c9657db95
Yuri Pieters <magejohnyjtp@gmail.com>
2021-10-14 14:06:05 +0100

    Sort packages

--- plugin/packages.lua
+++ plugin/packages.lua
@@ -1,107 +1,65 @@
 return require('packer').startup(function(use)
---> Editing
----> Language Server
-  use 'neovim/nvim-lspconfig'
-  use 'kabouzeid/nvim-lspinstall'
----> Completion
-  use 'hrsh7th/nvim-compe'
-  use {
-    'windwp/nvim-autopairs',
-    config = function()
-      require("nvim-autopairs").setup {
-        enable_check_bracket_line = true
-      }
-    end
-  }
----> Refactoring
-  use 'machakann/vim-sandwich'
-  use 'svermeulen/vim-subversive'
-  use {
-    'tpope/vim-abolish',
-    cmd = {'Abolish', 'Subvert', 'S'}
-  }
----> Text Objects
-  use 'wellle/targets.vim'
-  use 'kana/vim-textobj-user'
-  use 'kana/vim-textobj-entire'
-  use 'somini/vim-textobj-fold'
-  use 'kana/vim-textobj-syntax'
-  use 'bps/vim-textobj-python'
-  use 'kana/vim-textobj-indent'
   use 'Julian/vim-textobj-variable-segment'
----> Searching
+  use 'Konstruktionist/vim-fish'
+  use 'NoahTheDuke/vim-just'
+  use 'bps/vim-textobj-python'
+  use 'cespare/vim-toml'
+  use 'ddrscott/vim-window'
+  use 'enricobacis/vim-airline-clock'
   use 'haya14busa/is.vim'
-  use {
-      'haya14busa/vim-asterisk',
-      config = function() vim.g["asterisk#keeppos"] = true end
-  }
----> Miscellaneous
-  use 'tpope/vim-capslock'
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
-  }
+  use 'hrsh7th/nvim-compe'
+  use 'jvirtanen/vim-octave'
+  use 'kabouzeid/nvim-lspinstall'
+  use 'kana/vim-textobj-entire'
+  use 'kana/vim-textobj-indent'
+  use 'kana/vim-textobj-syntax'
+  use 'kana/vim-textobj-user'
+  use 'lambdalisue/gina.vim'
+  use 'lluchs/vim-wren'
+  use 'm-pilia/vim-pkgbuild'
+  use 'machakann/vim-sandwich'
+  use 'mhinz/vim-startify'
+  use 'neovim/nvim-lspconfig'
+  use 'somini/vim-textobj-fold'
   use 'svermeulen/vim-cutlass'
+  use 'svermeulen/vim-subversive'
   use 'svermeulen/vim-yoink'
-  use {'mbbill/undotree', cmd = "UndotreeToggle"}
-
---> Appearance
----> Colorscheme
-  use { 'MageJohn/base16-vim', opt = true }
-  use { 'mhartington/oceanic-next', opt = true }
----> Statusline
+  use 'tpope/vim-capslock'
   use 'vim-airline/vim-airline'
   use 'vim-airline/vim-airline-themes'
-  use 'enricobacis/vim-airline-clock'
----> Syntax highlighting
-  use 'lluchs/vim-wren'
-  use 'cespare/vim-toml'
-  use 'jvirtanen/vim-octave'
-  use 'NoahTheDuke/vim-just'
-  use 'Konstruktionist/vim-fish'
-  use 'm-pilia/vim-pkgbuild'
----> Miscellaneous
+  use 'wellle/targets.vim'
+  use { 'MageJohn/base16-vim', opt = true }
+  use { 'arithran/vim-delete-hidden-buffers', cmd = 'DeleteHiddenBuffers' }
   use { 'edluffy/specs.nvim', }
-
---> VCS integration
   use {
-    'mhinz/vim-signify',
-    event = { 'BufNewFile', 'BufRead' }
-  }
-  use 'lambdalisue/gina.vim'
-
---> Buffer management
-  use {
-    'arithran/vim-delete-hidden-buffers',
-    cmd = 'DeleteHiddenBuffers'
-  }
-  use {
-    'moll/vim-bbye',
-    cmd = { 'Bdelete', 'Bwipeout'}
-  }
-
---> Miscellaneous
-  use {
-    'tpope/vim-vinegar',
-    keys = '-',
-    ft = 'netrw'
-  }
-  use {
-    'tpope/vim-scriptease',
-    cmd = {'Messages', 'Vopen'}
-  }
-  use 'ddrscott/vim-window'
-  use 'mhinz/vim-startify'
-  use {
-    'kassio/neoterm',
-    cmd = {'T', 'Tnew' },
-    config = function()
-        vim.g.neoterm_default_mod = 'botright'
-    end
+    'haya14busa/vim-asterisk',
+    config = function() vim.g["asterisk#keeppos"] = true end
   }
   use {
     'iamcco/markdown-preview.nvim',
     ft = {'markdown', 'pandoc.markdown', 'rmd'},
     run = ':call mkdp#util#install()'
   }
+  use {
+    'kassio/neoterm',
+    cmd = {'T', 'Tnew' },
+    config = function() vim.g.neoterm_default_mod = 'botright' end
+  }
+  use { 'mhartington/oceanic-next', opt = true }
+  use { 'mhinz/vim-signify', event = { 'BufNewFile', 'BufRead' } }
+  use { 'moll/vim-bbye', cmd = { 'Bdelete', 'Bwipeout'} }
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+  }
+  use { 'tpope/vim-abolish', cmd = {'Abolish', 'Subvert', 'S'} }
+  use { 'tpope/vim-scriptease', cmd = {'Messages', 'Vopen'} }
+  use { 'tpope/vim-vinegar', keys = '-', ft = 'netrw' }
+  use {
+    'windwp/nvim-autopairs',
+    config = function()
+      require("nvim-autopairs").setup { enable_check_bracket_line = true }
+    end
+  }
+  use {'mbbill/undotree', cmd = "UndotreeToggle"}
 end)
